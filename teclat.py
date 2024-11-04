cec5c1001d97f54d476f1f26b40778a261404e9d
martimiro <marti.miro.06@gmail.com>
2024-11-04 10:55:52 +0100

    v2 de tecles negres

--- teclat.py
+++ teclat.py
@@ -22,16 +22,16 @@ main_label = tk.Label(window, text="Teclat piano", font=("Arial", 20))
 main_label.pack(pady=20)
 
 #Creem les tecles blanques d'un piano
-for num in range(octaves):
+for i in range(octaves):
     for nota in notes:
         button = tk.Button(canvas, text=nota, height=20, width=5)
         button.pack(pady=20, side=tk.LEFT)
 
 #Creem les tecles negres d'un piano
-for num in range(octaves):
+for z in range(octaves):
     for semi_nota in semi_notes:
             black_button = tk.Button(canvas, text=semi_nota, height=50, width=3, bg="black", fg="white")
-            black_button.place(x=50 + d_tecles_negres, y=100, width=30, height=120)
-            d_tecles_negres =+ 70
+            black_button.place(x = 50 + d_tecles_negres, y = 100, width = 30, height = 120)
+            d_tecles_negres = d_tecles_negres + 70
 
 window.mainloop()
