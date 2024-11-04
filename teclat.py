178f231154103ac73954640929480a31db652232
martimiro <marti.miro.06@gmail.com>
2024-11-04 17:52:02 +0100

    keyboard finished

--- teclat.py
+++ teclat.py
@@ -24,14 +24,22 @@ main_label.pack(pady=20)
 #Creem les tecles blanques d'un piano
 for i in range(octaves):
     for nota in notes:
-        button = tk.Button(canvas, text=nota, height=20, width=5)
+        button = tk.Button(canvas, text=nota, pady=30, height=20, width=5)
         button.pack(pady=20, side=tk.LEFT)
 
 #Creem les tecles negres d'un piano
 for z in range(octaves):
     for semi_nota in semi_notes:
-            black_button = tk.Button(canvas, text=semi_nota, height=50, width=3, bg="black", fg="white")
-            black_button.place(x = 50 + d_tecles_negres, y = 100, width = 30, height = 120)
-            d_tecles_negres = d_tecles_negres + 70
+
+            if semi_nota != semi_notes[1] and semi_nota != semi_notes[4]:
+                black_button = tk.Button(canvas, text=semi_nota, height=50, width=3, bg="black", fg="white")
+                black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
+                d_tecles_negres = d_tecles_negres + 70
+            
+            else:
+                black_button = tk.Button(canvas, text=semi_nota, height=50, width=3, bg="black", fg="white")
+                black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
+                d_tecles_negres = d_tecles_negres + 135
+
 
 window.mainloop()
