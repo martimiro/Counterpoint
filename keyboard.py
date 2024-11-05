1143ce6ec33a490d8546e954e3145bd393bef60d
martimiro <marti.miro.06@gmail.com>
2024-11-05 01:06:58 +0100

    fixed keyboard

--- keyboard.py
+++ keyboard.py
@@ -4,27 +4,15 @@ import wave
 import numpy as np
 import os
 
-'''
-fs = 44100  # Frecuencia de muestreo
-duration = 2.0  # Duración en segundos
-frequency = 440.0  # Frecuencia en Hz
 
-t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-wave_data = (0.5 * 32767 * np.sin(2 * np.pi * frequency * t)).astype(np.int16)
 
-with wave.open("output.wav", "wb") as f:
-    f.setnchannels(1)
-    f.setsampwidth(2)
-    f.setframerate(fs)
-    f.writeframes(wave_data.tobytes())
-
-os.system("output.wav")
-'''
 
 notes = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"]
 semi_notes = ["DO#\nREb", "RE#\nMIb", "FA#\nSOLb", "SOL#\nLAb", "LA#\nSIb"]
-
 l_notes = []
+
+frequencies = {"DO": 261.63, "DO#\nREb": 277.18, "RE": 293.66, "RE#\nMIb": 311.13, "MI": 329.63, "FA": 349.23, "FA#\nSOLb": 369.99, "SOL": 392.00,
+              "SOL#\nLAb": 415.30, "LA": 440.00, "LA#\nSIb": 466.16, "SI": 493.88}
 
 octaves = 2
 d_tecles_negres = 0
@@ -43,11 +31,39 @@ canvas.pack()
 main_label = tk.Label(window, text="Teclat piano", font=("Arial", 20))
 main_label.pack(pady=20)
 
-#Creem les tecles blanques d'un piano
+#Afegim les notes a la llista buida
 def get_notes(nota):
     l_notes.append(nota)
 
+#Fem que soni la llista
+def wav_list():
 
+    note_frequencies = [frequencies[note] for note in l_notes if note in frequencies]
+
+    fs = 44100 
+    duration = 2.0  # Durada en segons
+
+    full_wave_data = np.array([], dtype=np.int16)
+
+    for frequency in note_frequencies:
+        t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+        wave_data = (0.5 * 32767 * np.sin(2 * np.pi * frequency * t)).astype(np.int16)
+        full_wave_data = np.concatenate((full_wave_data, wave_data))
+
+
+    with wave.open("cantus.wav", "wb") as f:
+        f.setnchannels(1)
+        f.setsampwidth(2)
+        f.setframerate(fs)
+        f.writeframes(full_wave_data.tobytes())
+
+    os.system("cantus.wav")
+
+#Botó per fer play
+play_button = tk.Button(window, text=".wav amb el Cantus Firmus", command=wav_list)
+play_button.pack(pady=20)
+
+#Creem les tecles blanques d'un piano
 for i in range(octaves):
     for nota in notes:
         button = tk.Button(canvas, text=nota, pady=30, height=20, width=8, command=lambda nota=nota: get_notes(nota))
@@ -58,17 +74,16 @@ for z in range(octaves):
     for semi_nota in semi_notes:
             
             if semi_nota != semi_notes[1] and semi_nota != semi_notes[4]:
-                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white")
+                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white", 
+                                         command=lambda nota=semi_nota: get_notes(nota))
                 black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
                 d_tecles_negres = d_tecles_negres + 67
 
             #Espai entre Mi i Fa / Si i Do
             else:
-                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white")
+                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white", 
+                                         command=lambda nota=semi_nota: get_notes(nota))
                 black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
                 d_tecles_negres = d_tecles_negres + 130
 
-
 window.mainloop()
-
-print(l_notes)
