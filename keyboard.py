613148edb82cede44efc079c6d15ac67928161a6
martimiro <marti.miro.06@gmail.com>
2024-11-08 22:52:09 +0100

    fixed octave

--- keyboard.py
+++ keyboard.py
@@ -9,8 +9,8 @@ notes = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"]
 semi_notes = ["DO#\nREb", "RE#\nMIb", "FA#\nSOLb", "SOL#\nLAb", "LA#\nSIb"]
 l_notes = []
 
-frequencies = {"DO": 261.63, "DO#\nREb": 277.18, "RE": 293.66, "RE#\nMIb": 311.13, "MI": 329.63, "FA": 349.23, "FA#\nSOLb": 369.99, "SOL": 392.00,
-              "SOL#\nLAb": 415.30, "LA": 440.00, "LA#\nSIb": 466.16, "SI": 493.88}
+frequencies = {"DO": 130.813, "DO#\nREb": 138.591, "RE": 146.832, "RE#\nMIb": 155.563, "MI": 164.814, "FA": 174.614, "FA#\nSOLb": 184.997, "SOL": 195.998,
+              "SOL#\nLAb": 207.652, "LA": 220.000, "LA#\nSIb": 233.082, "SI": 246.942}
 
 octaves = 2
 d_tecles_negres = 0
@@ -29,8 +29,11 @@ def remove_notes():
     l_notes.clear()
 
 #Afegim les notes a la llista buida
-def get_notes(nota):
-    l_notes.append(nota)
+def get_notes(nota, octave = 1):
+    if octave == 2 and nota in frequencies:
+        l_notes.append((nota, frequencies[nota] * 2))
+    else:
+        l_notes.append((nota, frequencies[nota]))
 
 #Fem que soni la llista
 def wav_list():
@@ -50,7 +53,9 @@ def wav_list():
         boton_cerrar.pack(pady=5)
 
     else:
-        note_frequencies = [frequencies[note] for note in l_notes if note in frequencies]
+        
+        note_frequencies = [frequency for _, frequency in l_notes]
+
 
         fs = 44100 
         duration = 2.0  # Durada en segons
@@ -61,7 +66,7 @@ def wav_list():
             t = np.linspace(0, duration, int(fs * duration), endpoint=False)
             wave_data = (0.5 * 32767 * np.sin(2 * np.pi * frequency * t)).astype(np.int16)
             full_wave_data = np.concatenate((full_wave_data, wave_data))
-
+        
         with wave.open("cantus.wav", "wb") as f:
             f.setnchannels(1)
             f.setsampwidth(2)
@@ -87,23 +92,25 @@ play_button.pack(padx=10, pady=0, side=tk.LEFT)
 #Creem les tecles blanques d'un piano
 for i in range(octaves):
     for nota in notes:
-        button = tk.Button(canvas, text=nota, pady=30, height=20, width=8, command=lambda nota=nota: get_notes(nota))
+        octave = i + 1
+        button = tk.Button(canvas, text=nota, pady=30, height=20, width=8, command=lambda nota=nota, octave=octave: get_notes(nota, octave))                  
         button.pack(pady=20, side=tk.LEFT)
 
 #Creem les tecles negres d'un piano
 for z in range(octaves):
-    for semi_nota in semi_notes:
+        octave = z + 1
+
+        for semi_nota in semi_notes:
             
             if semi_nota != semi_notes[1] and semi_nota != semi_notes[4]:
-                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white", 
-                                         command=lambda nota=semi_nota: get_notes(nota))
+                black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white", command=lambda nota=semi_nota, octave=octave: get_notes(nota, octave))
                 black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
                 d_tecles_negres = d_tecles_negres + 67
 
             #Espai entre Mi i Fa / Si i Do
             else:
                 black_button = tk.Button(canvas, text=semi_nota, height=50, width=20, bg="black", fg="white", 
-                                         command=lambda nota=semi_nota: get_notes(nota))
+                         command=lambda nota=semi_nota, octave=octave: get_notes(nota, octave))
                 black_button.place(x = 50 + d_tecles_negres, y = 20, width = 38, height = 200)
                 d_tecles_negres = d_tecles_negres + 130
 
