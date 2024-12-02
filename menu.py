b4492e2a181e274e55daf2960e877480f43f6f65
martimiro <marti.miro.06@gmail.com>
2024-12-02 09:50:41 +0100

    open windown

---
+++ menu.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import messagebox
+
+import keyboard
+
+import keyboard
+
+#Creem la finestra
+window = tk.Tk()
+window.title("Menu")
+window.geometry("1200x800")
+
+#Funció per sortir de la finestra
+def sortir():
+    #Messagebox per confirmar que surti
+    exit = messagebox.askyesno(title="Sortir", message="Està segur que vol sortir?")
+
+    if exit == True:
+        window.destroy()
+
+#Obrim la finestra del Contrapunt 1:1
+
+
+#Creem el botó per sortir
+quit_button = tk.Button(window, text="Sortir", command=sortir)
+quit_button.pack(padx = 10, pady = 0, side = tk.LEFT)
+
+#Creem el botó per anar a la finestra del contrapunt 1:!
+contrapunt_1 = tk.Button(window, text = "Contrapunt 1:1", command = keyboard.contrapunt1_1)
+contrapunt_1.pack(padx  =10, pady = 0, side = tk.LEFT)
+
+window.mainloop()
